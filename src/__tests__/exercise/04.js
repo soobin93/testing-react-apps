3d82539f7fbc799a785eb4c51b3e7e85c9d008f4
Soobin Son <ssbin93@gmail.com>
2023-03-16 10:11:33 +1000

    do exercise 04

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -1,28 +1,48 @@
 // form testing
 // http://localhost:3000/login
 
-import * as React from 'react'
-import {render, screen} from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
-import Login from '../../components/login'
+import * as React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { faker } from '@faker-js/faker';
+import { build, perBuild } from '@jackfranklin/test-data-bot';
+import Login from '../../components/login';
 
-test('submitting the form calls onSubmit with username and password', () => {
-  // 🐨 create a variable called "submittedData" and a handleSubmit function that
-  // accepts the data and assigns submittedData to the data that was submitted
-  // 💰 if you need a hand, here's what the handleSubmit function should do:
-  // const handleSubmit = data => (submittedData = data)
-  //
-  // 🐨 render the login with your handleSubmit function as the onSubmit prop
-  //
-  // 🐨 get the username and password fields via `getByLabelText`
-  // 🐨 use `await userEvent.type...` to change the username and password fields to
-  //    whatever you want
-  //
-  // 🐨 click on the button with the text "Submit"
-  //
-  // assert that submittedData is correct
-  // 💰 use `toEqual` from Jest: 📜 https://jestjs.io/docs/en/expect#toequalvalue
-})
+const loginFormBuilder = build({
+  fields: {
+    username: perBuild(() => faker.internet.userName()),
+    password: perBuild(() => faker.internet.password())
+  }
+});
+
+const buildLoginForm = (overrides) => ({
+  ...loginFormBuilder.one(),
+  ...overrides
+});
+
+test('submitting the form calls onSubmit with username and password', async () => {
+  const handleSubmit = jest.fn();
+
+  render(<Login onSubmit={handleSubmit} />);
+
+  const userNameInput = screen.getByLabelText(/username/i);
+  const passwordInput = screen.getByLabelText(/password/i);
+
+  const { username, password } = buildLoginForm({ password: 'abc' });
+
+  await userEvent.type(userNameInput, username);
+  await userEvent.type(passwordInput, password);
+
+  const submitButton = screen.getByRole('button', { name: /submit/i });
+  await userEvent.click(submitButton);
+
+  expect(handleSubmit).toHaveBeenCalledWith({
+    username,
+    password
+  });
+
+  expect(handleSubmit).toHaveBeenCalledTimes(1);
+});
 
 /*
 eslint
